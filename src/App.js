cae2428a92a5a80cdafa9e50cedeb838f3d7624f
owen-mcmanus <owen.mcmanus727@gmail.com>
2021-09-12 20:26:23 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/home';
+import Ending from './pages/ending';
+import Game from './pages/game';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+						<Route path={'/game/:id'} component={Game} />
+						<Route path={'/end'} component={Ending} />
+						<Route path={'/'} component={Home} />
+						{/* <Route component={NotFound} /> */}
+					</Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    [
+        {
+            'title':'First Americans',
+            'questions':[
+                {'q':'The first peoples to inhabit the American continents are generally believed to have crossed the','a':'Beringia landmass'},
+                {'q':'This many years ago','a':'15,000'}
+            ],
+            'notes':['n/a']
+        },
+        {
+            'title':'Agricultural Revolution and Civilization Development',
+            'questions':[
+                {'q':'What 2 crops have longest recorded domestications?','a':'potato and maize'},
+                {'q':'Domesticated animals in the Americas include','a':'llamas, alpacas, and guinea pigs (raised for eating).'}
+            ],
+            'notes':['n/a']
+        }
+    ],
+    [],
+    []
+]
